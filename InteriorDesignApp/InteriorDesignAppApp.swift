91f4760ed4c73aa7c57d81ba66ed9ccc3385db91
Michelle Trinh <trinhmichelle23@gmail.com>
2024-03-11 23:28:21 -0700

    Initial Commit

---
+++ InteriorDesignApp/InteriorDesignAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteriorDesignAppApp.swift
+//  InteriorDesignApp
+//
+//  Created by Michelle Trinh on 3/11/24.
+//
+
+import SwiftUI
+
+@main
+struct InteriorDesignAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
