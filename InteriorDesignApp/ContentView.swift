da9740278ff7a6d17b0a668f6f59b00c486d8508
Michelle Trinh <trinhmichelle23@gmail.com>
2024-03-12 22:28:50 -0700

    adjusted image dimensions on home screen

--- InteriorDesignApp/ContentView.swift
+++ InteriorDesignApp/ContentView.swift
@@ -23,10 +23,16 @@ struct ContentView: View {
                     }
                 
                 ImageLoading(url: URL(string: "https://i.pinimg.com/564x/9f/11/54/9f11547adf8bec975060657f1fb10ddd.jpg")!)
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 200)
                 
                 ImageLoading(url: URL(string: "https://i.pinimg.com/564x/70/5b/b0/705bb0208ce7ce3c7aad03256343f33b.jpg")!)
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 200)
                 
                 ImageLoading(url: URL(string: "https://i.pinimg.com/564x/59/10/11/591011e5b5681201dee82b1ecce06a80.jpg")!)
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 200)
                 
                 
             
