76e6434a69fdecdb07623e52a3d514c1efcdc5e1
Michelle Trinh <trinhmichelle23@gmail.com>
2024-03-16 17:42:49 -0700

    added ScrollView to home screen

--- InteriorDesignApp/ContentView.swift
+++ InteriorDesignApp/ContentView.swift
@@ -11,33 +11,35 @@ struct ContentView: View {
     @State var designStyle = "Minimalistic"
     var body: some View {
         NavigationStack {
-            VStack {
-                Image(systemName: "house")
-                    .imageScale(.large)
-                    .foregroundStyle(.tint)
-                Text("your interior design style is: \(designStyle)")
-                    .navigationTitle("welcome home.")
-                    .navigationBarTitleDisplayMode(.inline)
-                    .toolbar {
-                        Button ("discover") {}
-                    }
-                
-                ImageLoading(url: URL(string: "https://i.pinimg.com/564x/9f/11/54/9f11547adf8bec975060657f1fb10ddd.jpg")!)
-                    .aspectRatio(contentMode: .fill)
-                    .frame(width: 200)
-                
-                ImageLoading(url: URL(string: "https://i.pinimg.com/564x/70/5b/b0/705bb0208ce7ce3c7aad03256343f33b.jpg")!)
-                    .aspectRatio(contentMode: .fill)
-                    .frame(width: 200)
-                
-                ImageLoading(url: URL(string: "https://i.pinimg.com/564x/59/10/11/591011e5b5681201dee82b1ecce06a80.jpg")!)
-                    .aspectRatio(contentMode: .fill)
-                    .frame(width: 200)
-                
-                
-            
+            ScrollView {
+                VStack {
+                    Image(systemName: "house")
+                        .imageScale(.large)
+                        .foregroundStyle(.tint)
+                    Text("your interior design style is: \(designStyle)")
+                        .navigationTitle("welcome home.")
+                        .navigationBarTitleDisplayMode(.inline)
+                        .toolbar {
+                            Button ("discover") {}
+                        }
+                    
+                    ImageLoading(url: URL(string: "https://i.pinimg.com/564x/9f/11/54/9f11547adf8bec975060657f1fb10ddd.jpg")!)
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 200)
+                    
+                    ImageLoading(url: URL(string: "https://i.pinimg.com/564x/70/5b/b0/705bb0208ce7ce3c7aad03256343f33b.jpg")!)
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 200)
+                    
+                    ImageLoading(url: URL(string: "https://i.pinimg.com/564x/59/10/11/591011e5b5681201dee82b1ecce06a80.jpg")!)
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 200)
+                    
+                    
+                    
+                }
+                .padding()
             }
-            .padding()
         }
     }
 }
