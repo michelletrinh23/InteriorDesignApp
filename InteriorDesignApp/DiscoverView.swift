c43cadca6bdc0c342cc21962c13614f6191ba85b
Michelle Trinh <trinhmichelle23@gmail.com>
2024-03-16 21:24:52 -0700

    DiscoverView rough draft
    the DiscoverView page now has one text prompt for the user and four buttons for the user to choose from.

--- InteriorDesignApp/DiscoverView.swift
+++ InteriorDesignApp/DiscoverView.swift
@@ -9,7 +9,48 @@ import SwiftUI
 
 struct DiscoverView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack {
+                Text("Click on the style you prefer")
+                
+                HStack {
+                    Button {
+                        
+                    } label: {
+                        ImageLoading(url: URL(string: "https://i.pinimg.com/564x/4d/44/ab/4d44ab82d0040b1a0fdf9e7bea4993ee.jpg")!)
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 200, height: 200)
+                    }
+                    
+                    Button {
+                        
+                    } label: {
+                        ImageLoading(url: URL(string: "https://i.pinimg.com/564x/52/8a/35/528a35e4d593ae8a97935b19c320663a.jpg")!)
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 200, height: 200)
+                    }
+                    
+                }
+                
+                HStack {
+                    Button {
+                        
+                    } label: {
+                        ImageLoading(url: URL(string: "https://i.pinimg.com/564x/3b/29/b7/3b29b70baface6eae4bdb4f376f74ba7.jpg")!)
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 200, height: 200)
+                    }
+                    
+                    Button {
+                        
+                    } label: {
+                        ImageLoading(url: URL(string: "https://i.pinimg.com/736x/38/55/9b/38559b1d0a75d845ab63582ad63e9e39.jpg")!)
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 200, height: 200)
+                    }
+                }
+            }
+        }
     }
 }
 
